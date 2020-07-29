9df35d728c70bb34358113d2a3f6be1523ed02fd
lfanew <tyler.bowers@outlook.com>
2020-07-28 19:50:54 -0500

    crafting recipe, auto window move and focus added

--- wurmbot/wurmbot.py
+++ wurmbot/wurmbot.py
@@ -1,8 +1,14 @@
 from client import WurmBot
+import pygetwindow as pgw
 import sys
 
 
 def main(args: list) -> int:
+    game_window = pgw.getWindowsWithTitle('Wurm Online')[0]
+
+    game_window.activate()
+    game_window.moveTo(-7,0)
+
     bot = WurmBot()
 
     bot.load(args[1])
